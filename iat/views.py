21b776eee98ede1a44b0112cf45c71bfba6a4604
Paul Girard <paul.girard@sciencespo.fr>
2016-02-04 10:58:38 +0100

    trying to implement IAT scenario into Otree

---
+++ iat/views.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+
+from otree.common import Currency as c, currency_range, safe_json
+
+from . import models
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+class IAT(Page):
+    pass
+
+
+page_sequence = [
+	IAT
+]
